17692b9fb4811eccfd67d74eb77de8ecdd810171
hyeonjilee <skylar12200@gmail.com>
2023-09-03 11:06:54 +0900

    cool@@

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,9 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
+    public static int solution(List<Integer> pobi2, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+        // cool!!
     }
 }
