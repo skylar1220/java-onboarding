d8f87ae58920c470d543c05db0784b7b7fb9f338
hyeonjilee <skylar12200@gmail.com>
2023-09-06 10:57:35 +0900

    refactor(Problem5) : 리팩토링, 테스트 코드 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,23 +1,12 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import problem5.MoneyChanging;
 
 public class Problem5 {
-
     public static List<Integer> solution(int money) {
-        List<Integer> moneyCountList = new ArrayList<>();
-        List<Integer> moneyList = List.of(50000, 10000, 5000, 1000, 500, 50, 10, 1);
-        int change = money;
-        int moneyCount = 0;
-        for (int i = 0; i < moneyList.size(); i++) {
-            moneyCount = change / moneyList.get(i);
-            change %= moneyList.get(i);
-            moneyCountList.add(moneyCount);
-        }
-        return moneyCountList;
+        List<Integer> answer = MoneyChanging.getMoneyCountList(money);
+        return answer;
     }
-
-
 }
