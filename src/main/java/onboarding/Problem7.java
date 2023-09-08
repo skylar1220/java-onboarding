cdf7fd1746620cf870bf1b74142bf285276518c8
hyeonjilee <skylar12200@gmail.com>
2023-09-08 19:04:12 +0900

    refactor(Problem7) : 기능 구현, 리팩토링

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,14 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import problem7.RecommandAlgorithm;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+
+    public static List<String> solution(String user, List<List<String>> friends,
+        List<String> visitors) {
+        RecommandAlgorithm recommandAlgorithm = new RecommandAlgorithm();
+        List<String> answer = recommandAlgorithm.getRecommandList(user, friends, visitors);
         return answer;
     }
 }
