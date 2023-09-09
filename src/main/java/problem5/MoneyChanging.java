eaa716a5199e67a5c341b02b4b22347aae9a349a
hyeonjilee <skylar12200@gmail.com>
2023-09-09 20:02:41 +0900

    refactor(Problem5): enum으로 반환한 리스트로 반복문 리팩토링

--- src/main/java/problem5/MoneyChanging.java
+++ src/main/java/problem5/MoneyChanging.java
@@ -4,31 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MoneyChanging {
-
-    private static List<Integer> MONEY_LIST = List.of(50000, 10000, 5000, 1000, 500, 50, 10, 1);
-    private static int INIT_LOOP_VALUE = 0;
-    private static int INIT_MONEY_COUNT = 0;
-
-    private static int getMoneyCount(int change, int moneyIndex) {
-        return change / MONEY_LIST.get(moneyIndex);
-    }
-
-    private static int getChange(int change, int moneyIndex) {
-        return change % MONEY_LIST.get(moneyIndex);
-    }
-
-    public static List<Integer> getMoneyCountList(int money) {
+    public List<Integer> getMoneyCountList(int money) {
         List<Integer> moneyCountList = new ArrayList<>();
-        int change = money;
-        int moneyCount = INIT_MONEY_COUNT;
-
-        for (int i = INIT_LOOP_VALUE; i < MONEY_LIST.size(); i++) {
-            moneyCount = getMoneyCount(change, i);
-            change = getChange(change, i);
-            moneyCountList.add(moneyCount);
+        for (Money m : Money.highestOrder()) {
+            int count = money / m.getValue();
+            moneyCountList.add(count);
+            money = money % m.getValue();
         }
         return moneyCountList;
     }
-
-
 }
--- src/test/java/problem5/MoneyChangingTest.java
+++ src/test/java/problem5/MoneyChangingTest.java
@@ -10,6 +10,6 @@ public class MoneyChangingTest {
     @Test
     void 화폐별_갯수_리스트_반환하는_기능(){
         List<Integer> result = Problem5.solution(7501);
-        assertThat(result).isEqualTo(List.of(0, 0, 1, 2, 1, 0, 0, 1));
+        assertThat(result).isEqualTo(List.of(0, 0, 1, 2, 1, 0, 0, 0, 1));
     }
 }
