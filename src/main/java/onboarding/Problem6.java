f99bed1ed6ca49386c5014bcc6abb7d14261de6e
hyeonjilee <skylar12200@gmail.com>
2023-09-09 10:55:20 +0900

    feat(Problem6): 1차 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import problem6.Checker;
+import problem6.ListConverter;
+import problem6.Member;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        ListConverter listConverter = new ListConverter();
+        List<Member> memberList = listConverter.getMemberList(forms);
+        Checker checker = new Checker(memberList);
+        List<Member> duplicationMemberList = checker.getDuplicationMemberList();
+        List<String> answer = listConverter.getEmailList(duplicationMemberList);
         return answer;
     }
 }
