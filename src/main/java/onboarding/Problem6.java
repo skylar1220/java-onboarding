7f3aa835d9a46a0d3546dd91906dd06bf8c3a881
hyeonjilee <skylar12200@gmail.com>
2023-09-10 17:59:00 +0900

    refactor(Problem6): stream 버전 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,14 @@
 package onboarding;
 
+import static onboarding.Problem6.addWordFromName;
+
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import problem6.Checker;
 import problem6.ListConverter;
 import problem6.Member;
@@ -14,5 +21,34 @@ public class Problem6 {
         memberList = new Checker(memberList).getDuplicatedList();
         List<String> answer = listConverter.getEmailList(memberList);
         return answer;
+    }
+
+    public static List<String> solution_stream(List<List<String>> forms) {
+        Map<String, List<String>> twoLetters = new HashMap<>();
+        forms.stream().forEach(form -> addWordFromName(twoLetters, getName(form), getEmail(form)));
+        List<String> result = twoLetters.values().stream()
+            .filter(emailList -> emailList.size() > 1)
+            .flatMap(Collection::stream)
+            .distinct()
+            .sorted()
+            .collect(Collectors.toList());
+        return result;
+    }
+
+    private static String getEmail(List<String> form) {
+        return form.get(0);
+    }
+
+    private static String getName(List<String> form) {
+        return form.get(1);
+    }
+
+    public static void addWordFromName(Map<String, List<String>> twoLetters, String name,
+        String email) {
+        String word = "";
+        for (int i = 0; i < name.length() - 1; i++) {
+            word = name.substring(i, i + 2);
+            twoLetters.computeIfAbsent(word, x -> new ArrayList<>()).add(email);
+        }
     }
 }
--- src/test/java/problem6/DuplicationCheckTest.java
+++ src/test/java/problem6/DuplicationCheckTest.java
@@ -20,4 +20,17 @@ public class DuplicationCheckTest {
         List<String> answer = Problem6.solution(forms);
         assertThat(answer).isEqualTo(List.of("1jm@email.com","2jason@email.com", "3hyeonji@email.com", "4leehyeonji@email.com"));
     }
+
+    @Test
+    void 스트림_버전(){
+        Problem6 problem6 = new Problem6();
+        List<List<String>> forms = new ArrayList<>();
+        forms.add(List.of("1jm@email.com", "1제이엠"));
+        forms.add(List.of("2jason@email.com", "2제이슨"));
+        forms.add(List.of("3hyeonji@email.com", "3현지야"));
+        forms.add(List.of("4leehyeonji@email.com", "4이현지"));
+        forms.add(List.of("5no@email.com", "5아님이"));
+        List<String> answer = Problem6.solution_stream(forms);
+        assertThat(answer).isEqualTo(List.of("1jm@email.com","2jason@email.com", "3hyeonji@email.com", "4leehyeonji@email.com"));
+    }
 }
